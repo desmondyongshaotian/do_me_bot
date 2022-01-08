fa9292deedd5aef7722dd31a6917241f0c0b3700
desmondyongshaotian <84946905+desmondyongshaotian@users.noreply.github.com>
2022-01-08 19:43:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -42,13 +42,13 @@ def start(message):
     chat_user = message.chat.title
   
   if chat_id in user:
-    bot.send_message(chat_id=chat_id, text='The Bot has already started')
+    bot.send_message(chat_id=chat_id, text='The bot has already started')
    return
 
-  else:
-    user[chat_id] = []
-    message_text = f'Hello {chat_user} !'
-    bot.reply_to(message, message_text)
+ 
+  user[chat_id] = []
+  message_text = f'Hello {chat_user} !'
+  bot.reply_to(message, message_text)
 
 
 @bot.message_handler(commands=['view'])
